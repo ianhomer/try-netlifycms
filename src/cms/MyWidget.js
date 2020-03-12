493228cd61a13c826c16f6ca2127ba0fc530faa4
Ian Homer <ian@purplepip.com>
2020-03-12 06:45:19 +0000

    Add custom widget

---
+++ src/cms/MyWidget.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export class MyWidgetControl extends Component {
+  render() {
+    return <div>My Custom Widget</div>;
+  }
+}
+
+// This is the preview component
+export const MyWidgetPreview = props => <div></div>;
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,13 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import QuestionPreview from './preview-templates/QuestionPreview'
 import InfoPagePreview from './preview-templates/InfoPagePreview'
+import { MyWidgetControl, MyWidgetPreview } from './MyWidget'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('info', InfoPagePreview)
 CMS.registerPreviewTemplate('question', QuestionPreview)
+
+CMS.registerWidget('myWidget', MyWidgetControl, MyWidgetPreview);
+
+console.log('Loaded custom CMS components')
+
