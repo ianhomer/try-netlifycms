80f507869fe497c139775d1d664f3a609f3df91f
Calum Brunt <cbrunt@calums-mbp.home>
2020-03-17 11:55:55 +0000

    Breaking change - add import netlify-cms-ui-default

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,6 +5,8 @@ import QuestionPreview from './preview-templates/QuestionPreview'
 import InfoPagePreview from './preview-templates/InfoPagePreview'
 import { MyWidgetControl, MyWidgetPreview } from './MyWidget'
 
+import { WidgetPreviewContainer } from "netlify-cms-ui-default"
+
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('info', InfoPagePreview)
 CMS.registerPreviewTemplate('question', QuestionPreview)
